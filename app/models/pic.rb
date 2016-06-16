4b0c338a32f0eadcb64ff9f580668f1c24d89479
Andrei Crudu <bogdancrudu@yahoo.com>
2016-06-17 01:46:13 +0300

    Add associations with pics

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,4 @@
 class Pic < ActiveRecord::Base
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615192958) do
+ActiveRecord::Schema.define(version: 20160616211700) do
 
   create_table "pics", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pics", ["user_id"], name: "index_pics_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
